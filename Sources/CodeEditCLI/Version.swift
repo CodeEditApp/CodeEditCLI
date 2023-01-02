917c9dcbc1da589b9469ee65dd542488406f9de8
Lukas Pistrol <lukas@pistrol.com>
2023-01-02 15:52:38 +0100

    fix version command

--- Sources/CodeEditCLI/Version.swift
+++ Sources/CodeEditCLI/Version.swift
@@ -16,23 +16,23 @@ extension CodeEditCLI {
         )
 
         func run() throws {
-            // Run an apple script to find CodeEdit.app
-            let pathData = try codeEditURLData()
+            // Print the cli version
+            print("CodeEditCLI: \t\(CLI_VERSION)")
+
+            // File URL of CodeEdit.app
+            let appURL = URL(fileURLWithPath: "/Applications/CodeEdit.app")
 
             // Check if there is an Info.plist inside CodeEdit.app
             // Then get the version number and print it out
             //
             // This will fail when CodeEdit.app is not installed
-            if let url = infoPlistUrl(pathData: pathData),
+            if let url = infoPlistUrl(appURL),
                let plist = NSDictionary(contentsOf: url) as? [String: Any],
                let version = plist["CFBundleShortVersionString"] as? String {
                 print("CodeEdit.app: \t\(version)")
             } else {
                 print("CodeEdit.app is not installed.")
             }
-
-            // Print the cli version
-            print("CodeEditCLI: \t\(CLI_VERSION)")
         }
 
         private func codeEditURLData() throws -> Data {
@@ -49,11 +49,9 @@ extension CodeEditCLI {
             return pipe.fileHandleForReading.readDataToEndOfFile()
         }
 
-        private func infoPlistUrl(pathData: Data) -> URL? {
-            if let path = String(data: pathData, encoding: .utf8) {
-                let url = URL(fileURLWithPath: path.trimmingCharacters(in: .whitespacesAndNewlines))
-                    .appendingPathComponent("Contents")
-                    .appendingPathComponent("Info.plist")
+        private func infoPlistUrl(_ url: URL?) -> URL? {
+            if let url = url?.appendingPathComponent("Contents")
+                             .appendingPathComponent("Info.plist") {
                 return url
             } else {
                 return nil
